9d44475f9f7d1d9fc46f57377ae5d3764391a68a
Sam Houghton <sam@3sidedcube.com>
2014-10-28 14:39:33 +0000

    Adds controls to video player

---
+++ ThunderCloud/TSCVideoPlayerControlsView.h
@@ -0,0 +1,17 @@
+//
+//  TSCVideoPlayerControlsView.h
+//  ThunderCloud
+//
+//  Created by Sam Houghton on 28/10/2014.
+//  Copyright (c) 2014 threesidedcube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSCVideoPlayerControlsView : UIView
+
+@property (nonatomic, strong) UISlider *volumeView;
+@property (nonatomic, strong) UIButton *playButton;
+@property (nonatomic, strong) UIButton *languageButton;
+
+@end
---
+++ ThunderCloud/TSCVideoPlayerControlsView.m
@@ -0,0 +1,58 @@
+//
+//  TSCVideoPlayerControlsView.m
+//  ThunderCloud
+//
+//  Created by Sam Houghton on 28/10/2014.
+//  Copyright (c) 2014 threesidedcube. All rights reserved.
+//
+
+#import "TSCVideoPlayerControlsView.h"
+
+@implementation TSCVideoPlayerControlsView
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+        self.backgroundColor = [UIColor colorWithRed:74.0f/255.0f green:75.0f/255.0f blue:77.0f/255.0f alpha:1.0];
+        
+        self.playButton = [UIButton new];
+        [self.playButton setImage:[UIImage imageNamed:@"mediaPauseButton" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
+        [self addSubview:self.playButton];
+        
+        self.languageButton = [UIButton new];
+        [self.languageButton setImage:[UIImage imageNamed:@"mediaLanguageButton" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
+        [self addSubview:self.languageButton];
+        
+        self.volumeView = [UISlider new];
+        [self.volumeView setThumbImage:[UIImage imageNamed:@"smallSlider" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
+        
+        [self addSubview:self.volumeView];
+        
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    
+    if(UIInterfaceOrientationIsPortrait(orientation)){
+        
+        self.playButton.frame = CGRectMake((self.frame.size.width / 2) - 50, 0, 40, 43);
+        self.languageButton.frame = CGRectMake((self.frame.size.width / 2) + 20, 0, 40, 43);
+        self.volumeView.frame = CGRectMake(44, self.bounds.size.height - 40, self.bounds.size.width - 88, 22);
+        
+    } else if(UIInterfaceOrientationIsLandscape(orientation)){
+        
+        self.playButton.frame = CGRectMake((self.center.x) - 20, 0, 40, 43);
+        self.languageButton.frame = CGRectMake(self.frame.size.width - 45, 0, 40, 43);
+        self.volumeView.frame = CGRectMake(20, self.bounds.size.height - 30, (self.bounds.size.width / 2) - 50, 22);
+        
+    }
+}
+
+@end
