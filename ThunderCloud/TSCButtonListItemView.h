6bada0b0390932299e2a12ad327c973930a57f42
Simon Mitchell <simon@3sidedcube.com>
2014-11-06 15:17:35 +0000

    Adds support for legacy TSCButtonListItemView

---
+++ ThunderCloud/TSCButtonListItemView.h
@@ -0,0 +1,13 @@
+//
+//  TSCButtonListItemView.h
+//  ThunderCloud
+//
+//  Created by Simon Mitchell on 06/11/2014.
+//  Copyright (c) 2014 threesidedcube. All rights reserved.
+//
+
+#import <ThunderCloud/ThunderCloud.h>
+
+@interface TSCButtonListItemView : TSCButtonListItem
+
+@end
---
+++ ThunderCloud/TSCButtonListItemView.m
@@ -0,0 +1,13 @@
+//
+//  TSCButtonListItemView.m
+//  ThunderCloud
+//
+//  Created by Simon Mitchell on 06/11/2014.
+//  Copyright (c) 2014 threesidedcube. All rights reserved.
+//
+
+#import "TSCButtonListItemView.h"
+
+@implementation TSCButtonListItemView
+
+@end
