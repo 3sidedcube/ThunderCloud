348defa250b88b2590ca73f16d8bb1470f859d7b
Matt Cheetham <matt@3sidedcube.com>
2014-11-11 09:12:30 +0000

    Adds documentation for TSCLink and TSCStormObjectDataSource

--- ThunderCloud/TSCLink.h
+++ ThunderCloud/TSCLink.h
@@ -8,19 +8,100 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ `TSCLink` is an object representation of a URL. This url can be a reference to a storm page, a website, details of an SMS, email and various other types.
+ 
+ Navigating between storm views is best handled using `TSCLink`
+ */
 @interface TSCLink : NSObject
 
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, strong) NSURL *url;
-@property (nonatomic, strong) NSString *linkClass;
-@property (nonatomic, strong) NSString *body;
-@property (nonatomic, strong) NSMutableArray *recipients;
-@property (nonatomic, strong) NSString *identifier;
-@property (nonatomic, strong) NSString *destination;
-@property (nonatomic, strong) NSNumber *duration;
-@property (nonatomic, strong) NSMutableArray *attributes;
+///---------------------------------------------------------------------------------------
+/// @name Initializing a TSCLink
+///---------------------------------------------------------------------------------------
 
-- (id)initWithDictionary:(NSDictionary *)dictionary;
+/**
+Initializes a TSCLink
+@param url The NSURL that you intend to push the user to
+*/
 - (id)initWithURL:(NSURL *)URL;
+
+/**
+Initializes a TSCLink
+@param dictionary A TSCLink dictionary that you intend to push the user to
+@discussion This dictionary can be found inside Storm's JSON files.
+*/
+- (id)initWithDictionary:(NSDictionary *)dictionary;
+
+///---------------------------------------------------------------------------------------
+/// @name Standard link properties
+///---------------------------------------------------------------------------------------
+
+/**
+ @abstract The title to describe the link
+ */
+@property (nonatomic, strong) NSString *title;
+
+/**
+ @abstract The URL given to the object after initialization
+ */
+@property (nonatomic, strong) NSURL *url;
+
+/**
+ @abstract The type of link
+ @discussion Storm has various link types for different link behaviours. They are represented as different objects in Storm but ultimately are represented by the same model in this iOS libary
+ */
+@property (nonatomic, strong) NSString *linkClass;
+
+///---------------------------------------------------------------------------------------
+/// @name SMS & Email Links
+///---------------------------------------------------------------------------------------
+
+/**
+ @abstract The body of text to be shared
+ @discussion Email and SMS links contain this body
+ */
+@property (nonatomic, strong) NSString *body;
+
+/**
+ @abstract An array of recipients that the body should be shared to
+ @discussion This property is only used by the SMS style link
+ */
+@property (nonatomic, strong) NSMutableArray *recipients;
+
+///---------------------------------------------------------------------------------------
+/// @name Inter-app linking
+///---------------------------------------------------------------------------------------
+
+/**
+ @abstract The unique identifier of the App as represented in the indentifiers.json
+ @discussion This is only used for inter-app linking
+ */
+@property (nonatomic, strong) NSString *identifier;
+
+/**
+ @abstract The URL to be passed to the recieving app
+ @discussion This is only used for inter-app linking
+ */
+@property (nonatomic, strong) NSString *destination;
+
+///---------------------------------------------------------------------------------------
+/// @name Timer Links
+///---------------------------------------------------------------------------------------
+
+/**
+ @abstract An NSNumber representation of the number of seconds the timer should run for
+ @discussion A TimerLink object uses this property
+ */
+@property (nonatomic, strong) NSNumber *duration;
+
+///---------------------------------------------------------------------------------------
+/// @name Miscellaneous properties
+///---------------------------------------------------------------------------------------
+
+/**
+ @abstract An array of additional attributed for the link
+ @discussion This array can contain any number of properties which may be useful when representing custom content
+ */
+@property (nonatomic, strong) NSMutableArray *attributes;
 
 @end
--- ThunderCloud/TSCStormObjectDataSource.h
+++ ThunderCloud/TSCStormObjectDataSource.h
@@ -8,10 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ All objects used in storm can optionally conform to the `TSCStormObjectDataSource`. This dataSource provides access to additional information about storm objects
+ */
 @protocol TSCStormObjectDataSource <NSObject>
 
+/**
+ @abstract An array of storm attributes used generally to describe additional customisation for an object
+ */
 - (NSArray *)stormAttributes;
+
+/**
+ @abstract A reference to the parent object of this storm object
+ */
 - (id)stormParentObject;
+
+/**
+ @abstract The setter for setting the parent storm object
+ */
 - (void)setStormParentObject:(id)parentObject;
 
 @end
