63046fea8443e3d0b80bd077374141e2ac8c783c
Sam Houghton <sam@3sidedcube.com>
2015-03-16 12:25:17 +0000

    Adds TSCStandardGridItem for legacy support

---
+++ ThunderCloud/TSCStandardGridItem.m
@@ -0,0 +1,13 @@
+//
+//  TSCStandardGridItem.m
+//  ThunderCloud
+//
+//  Created by Sam Houghton on 16/03/2015.
+//  Copyright (c) 2015 threesidedcube. All rights reserved.
+//
+
+#import "TSCStandardGridItem.h"
+
+@implementation TSCStandardGridItem
+
+@end
