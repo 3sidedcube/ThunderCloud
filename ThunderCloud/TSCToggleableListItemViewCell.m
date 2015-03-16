6d137e512a5deb2fb6b5502d55ccd9305d0963c1
Sam Houghton <sam@3sidedcube.com>
2015-03-16 10:57:09 +0000

    Updates toggle able list item cell to support right to left languages

--- ThunderCloud/TSCToggleableListItemViewCell.m
+++ ThunderCloud/TSCToggleableListItemViewCell.m
@@ -7,12 +7,13 @@
 //
 
 #import "TSCToggleableListItemViewCell.h"
+#import "TSCStormLanguageController.h"
 
-#define TEXT_LIST_ITEM_VIEW_TEXT_INSET 12
+#define TEXT_LIST_ITEM_VIEW_TEXT_INSET 16
 
 @interface TSCToggleableListItemViewCell ()
 
-@property (nonatomic, strong) NSString *detailsText;
+@property (nonatomic, copy) NSString *detailsText;
 @property UIButton *toggleButton;
 @property UIView *toggleContainerView;
 
@@ -20,8 +21,7 @@
 
 @implementation TSCToggleableListItemViewCell
 
-
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
     
@@ -42,37 +42,34 @@
 {
     [super layoutSubviews];
     
-    if([TSCThemeManager localisedTextDirectionForBaseDirection:NSTextAlignmentLeft] == NSTextAlignmentRight) {
-        
-        [self.toggleContainerView removeFromSuperview];
-        
-        [self.contentView addSubview:self.toggleContainerView];
-        self.toggleContainerView.frame = CGRectMake(14, 20, 11, self.frame.size.height - 28);
-        
-    } else {
-        
-        self.accessoryView = self.toggleContainerView;
-        self.toggleContainerView.frame = CGRectMake(self.toggleContainerView.frame.origin.x, self.toggleContainerView.frame.origin.y, self.toggleContainerView.frame.size.width, self.frame.size.height - 28);
-    }
+    self.accessoryView = self.toggleContainerView;
+    self.toggleContainerView.frame = CGRectMake(self.toggleContainerView.frame.origin.x, self.toggleContainerView.frame.origin.y, self.toggleContainerView.frame.size.width, self.frame.size.height - 28);
     
     CGSize size = [self.detailTextLabel sizeThatFits:CGSizeMake(self.frame.size.width - (TEXT_LIST_ITEM_VIEW_TEXT_INSET * 2), MAXFLOAT)];
     
+    self.textLabel.frame = CGRectMake(self.textLabel.frame.origin.x, 10, self.textLabel.frame.size.width, self.textLabel.frame.size.height);
+    
     if (self.isFullyVisible) {
-        
-        if (self.textLabel.frame.origin.y < 10) {
-            self.textLabel.frame = CGRectMake(self.textLabel.frame.origin.x, self.textLabel.frame.origin.y + 10, self.textLabel.frame.size.width, self.textLabel.frame.size.height);
-        }
-        
-        if([TSCThemeManager localisedTextDirectionForBaseDirection:NSTextAlignmentLeft] == NSTextAlignmentRight){
-            
-            self.detailTextLabel.frame = CGRectMake(-TEXT_LIST_ITEM_VIEW_TEXT_INSET, self.textLabel.frame.size.height + self.textLabel.frame.origin.y, size.width, size.height + TEXT_LIST_ITEM_VIEW_TEXT_INSET);
-        } else {
-            
-            self.detailTextLabel.frame = CGRectMake(TEXT_LIST_ITEM_VIEW_TEXT_INSET, self.textLabel.frame.size.height + self.textLabel.frame.origin.y, size.width, size.height + TEXT_LIST_ITEM_VIEW_TEXT_INSET);
-        }
+        self.detailTextLabel.frame = CGRectMake(TEXT_LIST_ITEM_VIEW_TEXT_INSET, self.textLabel.frame.size.height + self.textLabel.frame.origin.y, self.frame.size.width - (TEXT_LIST_ITEM_VIEW_TEXT_INSET * 2), size.height + TEXT_LIST_ITEM_VIEW_TEXT_INSET);
     }
     
-    [self.detailTextLabel setFont:[UIFont systemFontOfSize:14]];
+    if([[TSCStormLanguageController sharedController] isRightToLeft] && [self isMemberOfClass:[TSCToggleableListItemViewCell class]]) {
+        
+        for (UIView *view in self.contentView.subviews) {
+            
+            view.frame = CGRectMake(self.frame.size.width - view.frame.origin.x - view.frame.size.width, view.frame.origin.y, view.frame.size.width, view.frame.size.height);
+            
+            if([view isKindOfClass:[UILabel class]]) {
+                
+                ((UILabel *)view).textAlignment = NSTextAlignmentRight;
+                
+            }
+            
+        }
+        
+        self.accessoryView.frame = CGRectMake(self.frame.size.width - self.accessoryView.frame.origin.x - self.accessoryView.frame.size.width, self.accessoryView.frame.origin.y, self.accessoryView.frame.size.width, self.accessoryView.frame.size.height);
+        
+    }
 }
 
 - (void)setIsFullyVisible:(BOOL)isFullyVisible
@@ -81,18 +78,10 @@
     if (!_isFullyVisible) {
         
         self.detailTextLabel.text = @"";
-        if ([TSCThemeManager isOS8]) {
-            [self.toggleButton setImage:[UIImage imageNamed:@"chevron-down" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
-        } else {
-            [self.toggleButton setImage:[UIImage imageNamed:@"chevron-down"] forState:UIControlStateNormal];
-        }
+        [self.toggleButton setImage:[UIImage imageNamed:@"chevron-down"] forState:UIControlStateNormal];
     } else {
         
-        if ([TSCThemeManager isOS8]) {
-            [self.toggleButton setImage:[UIImage imageNamed:@"chevron-up" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
-        } else {
-            [self.toggleButton setImage:[UIImage imageNamed:@"chevron-up"] forState:UIControlStateNormal];
-        }
+        [self.toggleButton setImage:[UIImage imageNamed:@"chevron-up"] forState:UIControlStateNormal];
     }
 }
 
