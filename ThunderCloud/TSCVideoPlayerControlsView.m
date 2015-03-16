f51f7afdd08b325f059dcbd2497d4e0857d79dfc
Sam Houghton <sam@3sidedcube.com>
2015-03-16 11:41:47 +0000

    Updates TSCVideoPlayerControlsView layout

--- ThunderCloud/TSCVideoPlayerControlsView.m
+++ ThunderCloud/TSCVideoPlayerControlsView.m
@@ -18,29 +18,16 @@
         self.backgroundColor = [UIColor colorWithRed:74.0f/255.0f green:75.0f/255.0f blue:77.0f/255.0f alpha:1.0];
         
         self.playButton = [UIButton new];
-        
-        if ([TSCThemeManager isOS8]) {
-            [self.playButton setImage:[UIImage imageNamed:@"mediaPauseButton" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
-        } else {
-            [self.playButton setImage:[UIImage imageNamed:@"mediaPauseButton"] forState:UIControlStateNormal];
-        }
+        [self.playButton setImage:[UIImage imageNamed:@"mediaPauseButton"] forState:UIControlStateNormal];
         [self addSubview:self.playButton];
         
         self.languageButton = [UIButton new];
-        if ([TSCThemeManager isOS8]) {
-            [self.languageButton setImage:[UIImage imageNamed:@"mediaLanguageButton" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
-        } else {
-            [self.languageButton setImage:[UIImage imageNamed:@"mediaLanguageButton"] forState:UIControlStateNormal];
-        }
+        [self.languageButton setImage:[UIImage imageNamed:@"mediaLanguageButton"] forState:UIControlStateNormal];
         [self addSubview:self.languageButton];
         
         self.volumeView = [UISlider new];
+        [self.volumeView setThumbImage:[UIImage imageNamed:@"smallSlider"] forState:UIControlStateNormal];
         
-        if ([TSCThemeManager isOS8]) {
-            [self.volumeView setThumbImage:[UIImage imageNamed:@"smallSlider" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
-        } else {
-            [self.volumeView setThumbImage:[UIImage imageNamed:@"smallSlider"] forState:UIControlStateNormal];
-        }
         [self addSubview:self.volumeView];
     }
     
