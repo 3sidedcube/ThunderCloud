7985afd907b59b82f679515d1b109459738c2dd9
Sam Houghton <sam@3sidedcube.com>
2015-03-16 10:52:04 +0000

    Updates unordered list item cell layout

--- ThunderCloud/TSCUnorderedListItemViewCell.m
+++ ThunderCloud/TSCUnorderedListItemViewCell.m
@@ -7,10 +7,11 @@
 //
 
 #import "TSCUnorderedListItemViewCell.h"
+@import ThunderBasics;
 
 @implementation TSCUnorderedListItemViewCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
         
@@ -18,7 +19,8 @@
         self.bulletView.backgroundColor = [[TSCThemeManager sharedTheme] mainColor];
         [self.contentView addSubview:self.bulletView];
         self.indentationWidth = 5;
-        self.indentationLevel = 1;
+        self.indentationLevel = 1;        
+        self.bulletView.layer.cornerRadius = 5;
     }
     
     return self;
@@ -28,9 +30,9 @@
 {
     [super layoutSubviews];
     
-    self.bulletView.frame = CGRectMake(15, 5, 5, self.contentView.bounds.size.height - 10);
+    self.bulletView.frame = CGRectMake(34, 16, 10, 10);
     
-    CGPoint textOffset = CGPointMake(self.bulletView.frame.size.width + self.bulletView.frame.origin.x + 10, 10);
+    CGPoint textOffset = CGPointMake(self.bulletView.frame.size.width + self.bulletView.frame.origin.x + 22, 10);
     CGSize constainedSize = CGSizeMake(self.contentView.frame.size.width - textOffset.x - 12, MAXFLOAT);
     
     CGSize textLabelSize = [self.textLabel sizeThatFits:constainedSize];
@@ -38,6 +40,13 @@
     
     self.textLabel.frame = CGRectMake(textOffset.x, textOffset.y, textLabelSize.width, textLabelSize.height);
     self.detailTextLabel.frame = CGRectMake(textOffset.x, self.textLabel.frame.size.height + self.textLabel.frame.origin.y + 5, detailTextLabelSize.width, detailTextLabelSize.height);
+    
+    [self.bulletView setCenterY:self.textLabel.frame.origin.y + self.textLabel.frame.size.height/2];
+    
+    if (!self.detailTextLabel.text) {
+        [self.textLabel setCenterY:self.contentView.frame.size.height/2];
+        [self.bulletView setCenterY:self.contentView.frame.size.height/2];
+    }
 }
 
 @end
