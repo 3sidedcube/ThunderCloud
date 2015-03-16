5beb8adced61f27414fbabdc13e3e258051cbd37
Sam Houghton <sam@3sidedcube.com>
2015-03-16 11:07:56 +0000

    Updates app linking to support different languages

--- ThunderCloud/TSCAppIdentity.m
+++ ThunderCloud/TSCAppIdentity.m
@@ -7,22 +7,34 @@
 //
 
 #import "TSCAppIdentity.h"
+#import "TSCStormLanguageController.h"
 
 @implementation TSCAppIdentity
 
-- (id)initWithDictionary:(NSDictionary *)dictionary
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
 {
     return [self initWithDictionary:dictionary parentObject:nil];
 }
 
 
-- (id)initWithDictionary:(NSDictionary *)dictionary parentObject:(id)parentObject
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary parentObject:(id)parentObject
 {
     if (self = [super init]) {
+        
         self.appIdentifier = dictionary[@"appIdentifier"];
         self.iTunesId = dictionary[@"ios"][@"iTunesId"];
         self.countryCode = dictionary[@"ios"][@"countryCode"];
         self.launcher = dictionary[@"ios"][@"launcher"];
+        
+        if (dictionary[@"name"]) {
+            
+            NSString *shortLanguageString = [[TSCStormLanguageController sharedController] currentLanguageShortKey];
+            self.appName = dictionary[@"name"][shortLanguageString];
+            
+            if (!self.appName) {
+                self.appName = dictionary[@"name"][@"en"];
+            }
+        }
     }
     
     return self;
