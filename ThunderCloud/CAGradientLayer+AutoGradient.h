3a3d36cd72a7189992105fbde66f261b25cc9255
Sam Houghton <sam@3sidedcube.com>
2015-03-17 14:35:34 +0000

    Adds helper documentation for helper we have written

--- ThunderCloud/CAGradientLayer+AutoGradient.h
+++ ThunderCloud/CAGradientLayer+AutoGradient.h
@@ -9,8 +9,16 @@
 @import QuartzCore;
 @import UIKit;
 
+/**
+ `CAGradientLayer+AutoGradient` is a category that generates `CAGradientLayer`s easily
+ */
 @interface CAGradientLayer (AutoGradient)
 
+/**
+ Returns a `CAGradientLayer` transitioning from one color to another. A transparent color can be parsed to create dark to translucent gradients.
+ @param topColor The gradients top color represented as a `UIColor`. Starts at location 0.0
+ @param bottomColor The gradients bottom color represented as a `UIColor`. Starts at location 1.0
+ */
 + (CAGradientLayer *)generateGradientLayerWithTopColor:(UIColor *)topColor bottomColor:(UIColor *)bottomColor;
 
 @end
