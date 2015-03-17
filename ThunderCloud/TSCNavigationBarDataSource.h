57f03e8f452cdd3c1002e893ce8faf8a0f13b7ad
Sam Houghton <sam@3sidedcube.com>
2015-03-17 15:44:32 +0000

    Adds documentation for navigation

--- ThunderCloud/TSCNavigationBarDataSource.h
+++ ThunderCloud/TSCNavigationBarDataSource.h
@@ -8,9 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Any `UIViewController` can comply to this delegate. `UINavigationController+TSCNavigationController` uses this method to determine whether the navigation bar should be tranparent or not.
+ */
 @protocol TSCNavigationBarDataSource <NSObject>
 
 @optional
+/**
+ Can be used in any `UIViewController` to determine whether the navigation bar should be tranparent or not. If the return value is changed call `- setNeedsNavigationBarAppearanceUpdateAnimated:animated` on the navigation controller to update the navigation bars appearence.
+ */
 - (BOOL)shouldHideNavigationBar;
 
 @end
--- ThunderCloud/UINavigationController+TSCNavigationController.h
+++ ThunderCloud/UINavigationController+TSCNavigationController.h
@@ -10,14 +10,46 @@
 
 @class TSCLink;
 
+/**
+ `UINavigationController+TSCNavigationController` is a category that provides convenience methods related to navigation
+ */
 @interface UINavigationController (TSCNavigationController)
 
+/**
+ Returns a shared instance of the `UINavigationController`
+ */
 + (UINavigationController *)sharedController;
 
+/**
+ Performs an action depending on the `TSCLink` type
+ @param link A `TSCLink` to decide which action to perform
+ */
 - (void)pushLink:(TSCLink *)link;
+
+/**
+ Pushes a `TSCMultiVideoPlayerViewController` player on to the screen with an array of `TSCVideo`s
+ @param videos An array of `TSCVideo`s
+ */
 - (void)pushVideos:(NSArray *)videos;
+
+/**
+ Registers a native link which is used to push to a native page within the app
+ @param nativeLinkName The name of the native link
+ @param viewControllerClass The class that should be pushed when the link is used
+ */
 + (void)registerNativeLink:(NSString *)nativeLinkName toViewControllerClass:(Class)viewControllerClass;
+
+/**
+ Pushes a native view controller on to the navigation stack
+ @param nativeViewController The native view controller to be pushed on to the stack
+ @param animated Specify YES to animate the transition or NO if you do not want the transition to be animated. You might specify NO if you are setting up the navigation controller at launch time.
+ */
 - (void)pushNativeViewController:(UIViewController *)nativeViewController animated:(BOOL)animated;
+
+/**
+ Reloads the navigation bar appearence. Used if a view needs to switch between transparency e.g. When scrolling down a view you might want the navigation bar to become opaque
+ @param animated If the animated is set to YES the navigation bar will animate its alpha change
+ */
 - (void)setNeedsNavigationBarAppearanceUpdateAnimated:(BOOL)animated;
 
 @end
