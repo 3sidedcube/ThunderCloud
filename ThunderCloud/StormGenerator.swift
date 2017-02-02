e7fe6ce8b0e625c8d194dd0cef74315098a358ba
Matthew Cheetham <matt@3sidedcube.com>
2017-02-02 15:41:12 +0000

    Adds ability to load storm pages by name

--- ThunderCloud/StormGenerator.swift
+++ ThunderCloud/StormGenerator.swift
@@ -11,16 +11,30 @@ import Foundation
 /// A Swift replacement for TSCStormViewController. Generates view controllers from URL's, page ID's and names and returns an optional `UIViewController` so that it can be type checked against custom types.
 public class StormGenerator {
     
+    /// Turns a storm page name (Internal system name) into a view controller
+    ///
+    /// - Parameter name: The internal system name for the page to generate
+    /// - Returns: An optional `UIViewController` that may be a subclass of UIViewController. Most likely `TSCListPage`
+    public class func viewController(name: String) -> UIViewController? {
+        
+        let pageMetaDataDictionary = ContentController.shared.metadataForPage(withName: name)
+        
+        if let _pageMetaDataDictionary = pageMetaDataDictionary, let pageSrc = _pageMetaDataDictionary["src"] as? String, let pageURL = URL(string: pageSrc) {
+            return StormGenerator.viewController(URL: pageURL)
+        }
+        return nil
+    }
+    
     /// Turns a storm page ID into a view controller
     ///
     /// - Parameter identifier: The storm page ID to convert into a view controller
     /// - Returns: An optional `UIViewController` that may be a subclass of UIViewController. Most likely `TSCListPage`
-    public class func viewController(with identifier: String) -> UIViewController? {
+    public class func viewController(identifier: String) -> UIViewController? {
         
         let cacheURL = URL(string: "cache://pages/\(identifier).json")
         
         if let _cacheURL = cacheURL {
-            return StormGenerator.viewController(with: _cacheURL)
+            return StormGenerator.viewController(URL: _cacheURL)
         }
         return nil
     }
@@ -29,7 +43,7 @@ public class StormGenerator {
     ///
     /// - Parameter URL: The cache URL to convert (e.g. "cache://pages/123.json")
     /// - Returns: An optional `UIViewController` that may be a subclass of UIViewController. Most likely `TSCListPage`
-    public class func viewController(with URL: URL) -> UIViewController? {
+    public class func viewController(URL: URL) -> UIViewController? {
         
         guard let type = URL.host else {
             return nil
@@ -47,7 +61,6 @@ public class StormGenerator {
             
             return TSCStormObject.object(with: jsonObject, parentObject: nil) as? UIViewController
         }
-        
         return nil
     }
 }
