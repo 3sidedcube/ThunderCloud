e3d5ef426fd44340f7772ebce6757dc32ed3e67d
Joel Trew <joel@3sidedcube.com>
2017-09-18 15:59:14 +0100

    Implements Localised Links on TSCLink with a helper function

--- ThunderCloud/TSCLink+Localised.swift
+++ ThunderCloud/TSCLink+Localised.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+public extension TSCLink {
+    
+    public func linkFromLocalisedLink(with dictionary: [AnyHashable: Any]) {
+        
+        guard let urlDictionaries = dictionary["links"] as? [[AnyHashable: Any]] else { return }
+        
+        guard let urlType = dictionary["type"] as? String else { return }
+        
+        self.linkClass = urlType
+        
+        let urls = urlDictionaries.flatMap({ LocalisedLinkContents(from: $0) })
+
+        let selectedLink = urls.filter({ (linkContent) -> Bool in
+
+            let languagePack = StormLanguageController.shared.languagePack(forLocaleIdentifier: linkContent.localeIdentifier)
+           return languagePack?.fileName == StormLanguageController.shared.currentLanguage
+        }).first
+        
+        
+        // Set the TSCLink's url property to the selected locales url, if that doesn't exist fall back to the first link or nil if that doens't exist
+        if let selectedLink = selectedLink {
+            self.url = selectedLink.src
+        } else {
+            self.url = urls.first?.src
+        }
+    }
+    
+    
+    struct LocalisedLinkContents {
+        
+        var className: String?
+        var src: URL
+        var localeIdentifier: String
+        
+        init?(from dictionary: [AnyHashable: Any]) {
+            
+            self.className = dictionary["class"] as? String
+            
+            guard let srcString = dictionary["src"] as? String else {
+                return nil
+            }
+            guard let src = URL(string: srcString) else {
+                return nil
+            }
+            self.src = src
+            
+            guard let localeString = dictionary["locale"] as? String else {
+                return nil
+            }
+            
+            self.localeIdentifier = localeString
+        }
+    }
+}
