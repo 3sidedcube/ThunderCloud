1010b7aea9f9d5b775bd3a6155cde6fc9a706185
Simon Mitchell <simon@3sidedcube.com>
2019-03-12 14:05:26 +0000

    Re-indents edited files

--- ThunderCloud/CollectionListItem.swift
+++ ThunderCloud/CollectionListItem.swift
@@ -11,204 +11,204 @@ import ThunderTable
 
 /// Defines what the `CollectionListItem` is displaying
 public enum CollectionListItemType {
-	
-	/// Collection view is displaying quiz badges
-	case quiz
-	/// Collection view is displaying apps
-	case app
-	/// Collection view is displaying links
-	case link
-	/// Collection view is displaying generic badges
-	case badge
-	/// An unknown list of items
-	case unknown
+    
+    /// Collection view is displaying quiz badges
+    case quiz
+    /// Collection view is displaying apps
+    case app
+    /// Collection view is displaying links
+    case link
+    /// Collection view is displaying generic badges
+    case badge
+    /// An unknown list of items
+    case unknown
 }
 
 /// A storm item representing a collection of items displayed in a horizontal UICollectionView
 open class CollectionListItem: ListItem {
-
-	/// Defines what the collection list item is displaying
-	public var type: CollectionListItemType = .unknown
-	
-	/// The array of badges to display in the collection
-	public var badges: [Badge]?
-	
-	/// The array of apps to display in the collection
-	public var apps: [AppCollectionItem]?
-	
-	/// The array of links to display in the collection
-	public var links: [LinkCollectionItem]?
-	
-	/// The array of quizzes to display in the collection
-	public var quizzes: [Quiz]?
-	
-	private var quizCompletetionObserver: NSObjectProtocol?
-	
-	deinit {
-		if let quizCompletionObserver = quizCompletetionObserver {
-			NotificationCenter.default.removeObserver(quizCompletionObserver)
-		}
+    
+    /// Defines what the collection list item is displaying
+    public var type: CollectionListItemType = .unknown
+    
+    /// The array of badges to display in the collection
+    public var badges: [Badge]?
+    
+    /// The array of apps to display in the collection
+    public var apps: [AppCollectionItem]?
+    
+    /// The array of links to display in the collection
+    public var links: [LinkCollectionItem]?
+    
+    /// The array of quizzes to display in the collection
+    public var quizzes: [Quiz]?
+    
+    private var quizCompletetionObserver: NSObjectProtocol?
+    
+    deinit {
+        if let quizCompletionObserver = quizCompletetionObserver {
+            NotificationCenter.default.removeObserver(quizCompletionObserver)
+        }
         quizCompletetionObserver = nil
-	}
-	
-	public required init(dictionary: [AnyHashable : Any]) {
-		
-		super.init(dictionary: dictionary)
-		
-		guard let collectionCells = dictionary["cells"] as? [[AnyHashable : Any]], let cellClass = collectionCells.first?["class"] as? String else { return }
-		
-		switch cellClass {
-			case "QuizCollectionItem", "QuizCollectionCell":
-				type = .quiz
-				setupQuizzes(with: collectionCells)
-			break
-			case "AppCollectionItem", "AppCollectionCell":
-				type = .app
-				apps = collectionCells.map({ (collectionCell) -> AppCollectionItem in
-					return AppCollectionItem(dictionary: collectionCell)
-				})
-			break
-			case "LinkCollectionItem", "LinkCollectionCell":
-				type = .link
-				links = collectionCells.compactMap({ (collectionCell) -> LinkCollectionItem? in
-					return LinkCollectionItem(dictionary: collectionCell)
-				})
-			break
-			case "BadgeCollectionCell", "BadgeCollectionItem":
-				type = .badge
-				setupBadges(with: collectionCells)
-			default:
-				print("[STORM] Unknown cell type set on CollectionListItem")
-		}
-	}
-	
-	private func setupQuizzes(with items: [[AnyHashable : Any]]) {
-		
-		var _badges: [Badge] = []
-		var _quizzes: [Quiz] = []
-		
-		items.forEach { (quizCell) in
-			
-			// Make sure qe have a ["quiz"]["destination"] and a badge for the quiz
-			guard let quizDestination = (quizCell["quiz"] as? [AnyHashable : Any])?["destination"] as? String, let badgeId = quizCell["badgeId"], let badge = BadgeController.shared.badge(for: "\(badgeId)") else {
-				return
-			}
-			
-			// Load the Quiz object
-			guard let quizURL = URL(string: quizDestination) else {
-				return
-			}
-			guard let quiz = StormGenerator.quiz(for: quizURL) else {
-				return
-			}
-			
-			quiz.badgeId = badge.id
-			_badges.append(badge)
-			_quizzes.append(quiz)
-		}
-		
-		badges = _badges
-		quizzes = _quizzes
-		
-		quizCompletetionObserver = NotificationCenter.default.addObserver(forName: QUIZ_COMPLETED_NOTIFICATION, object: nil, queue: .main) { [weak self] (notification) -> Void in
-			self?.handleQuizCompletion()
-		}
-	}
-	
-	private func setupBadges(with items: [[AnyHashable : Any]]) {
-		
-		badges = items.compactMap({ (badgeCell) -> Badge? in
-			
-			var badgeId: String? = badgeCell["badgeId"] as? String
-			if let badgeNumberId = badgeCell["badgeId"] as? Int {
-				badgeId = "\(badgeNumberId)"
-			}
-			
-			guard let _badgeId = badgeId else { return nil }
-			return BadgeController.shared.badge(for: _badgeId)
-		})
-	}
-	
-	private func handleQuizCompletion() {
+    }
+    
+    public required init(dictionary: [AnyHashable : Any]) {
+        
+        super.init(dictionary: dictionary)
+        
+        guard let collectionCells = dictionary["cells"] as? [[AnyHashable : Any]], let cellClass = collectionCells.first?["class"] as? String else { return }
+        
+        switch cellClass {
+        case "QuizCollectionItem", "QuizCollectionCell":
+            type = .quiz
+            setupQuizzes(with: collectionCells)
+            break
+        case "AppCollectionItem", "AppCollectionCell":
+            type = .app
+            apps = collectionCells.map({ (collectionCell) -> AppCollectionItem in
+                return AppCollectionItem(dictionary: collectionCell)
+            })
+            break
+        case "LinkCollectionItem", "LinkCollectionCell":
+            type = .link
+            links = collectionCells.compactMap({ (collectionCell) -> LinkCollectionItem? in
+                return LinkCollectionItem(dictionary: collectionCell)
+            })
+            break
+        case "BadgeCollectionCell", "BadgeCollectionItem":
+            type = .badge
+            setupBadges(with: collectionCells)
+        default:
+            print("[STORM] Unknown cell type set on CollectionListItem")
+        }
+    }
+    
+    private func setupQuizzes(with items: [[AnyHashable : Any]]) {
+        
+        var _badges: [Badge] = []
+        var _quizzes: [Quiz] = []
+        
+        items.forEach { (quizCell) in
+            
+            // Make sure qe have a ["quiz"]["destination"] and a badge for the quiz
+            guard let quizDestination = (quizCell["quiz"] as? [AnyHashable : Any])?["destination"] as? String, let badgeId = quizCell["badgeId"], let badge = BadgeController.shared.badge(for: "\(badgeId)") else {
+                return
+            }
+            
+            // Load the Quiz object
+            guard let quizURL = URL(string: quizDestination) else {
+                return
+            }
+            guard let quiz = StormGenerator.quiz(for: quizURL) else {
+                return
+            }
+            
+            quiz.badgeId = badge.id
+            _badges.append(badge)
+            _quizzes.append(quiz)
+        }
+        
+        badges = _badges
+        quizzes = _quizzes
+        
+        quizCompletetionObserver = NotificationCenter.default.addObserver(forName: QUIZ_COMPLETED_NOTIFICATION, object: nil, queue: .main) { [weak self] (notification) -> Void in
+            self?.handleQuizCompletion()
+        }
+    }
+    
+    private func setupBadges(with items: [[AnyHashable : Any]]) {
+        
+        badges = items.compactMap({ (badgeCell) -> Badge? in
+            
+            var badgeId: String? = badgeCell["badgeId"] as? String
+            if let badgeNumberId = badgeCell["badgeId"] as? Int {
+                badgeId = "\(badgeNumberId)"
+            }
+            
+            guard let _badgeId = badgeId else { return nil }
+            return BadgeController.shared.badge(for: _badgeId)
+        })
+    }
+    
+    private func handleQuizCompletion() {
         parentViewController?.tableView?.reloadData()
-	}
-	
-	//MARK: -
-	//MARK: Row protocol
-	//MARK: -
-	
-	override open func configure(cell: UITableViewCell, at indexPath: IndexPath, in tableViewController: TableViewController) {
-		
-		super.configure(cell: cell, at: indexPath, in: tableViewController)
-		
-		switch type {
-		case .quiz:
-			guard let quizBadgeScrollerCell = cell as? QuizBadgeScrollerViewCell else { return }
-			quizBadgeScrollerCell.quizzes = quizzes
-			quizBadgeScrollerCell.badges = badges
-		break
-		case .app:
-			guard let appCollectionCell = cell as? AppCollectionCell else { return }
-			appCollectionCell.apps = apps
-		break
-		case .link:
-			guard let linkCollectionCell = cell as? LinkCollectionCell else { return }
-			linkCollectionCell.links = links
-		break
-		case .badge:
-			guard let badgeScrollerCell = cell as? BadgeScrollerViewCell else { return }
-			badgeScrollerCell.badges = badges
-		default: break
-		}
-	}
-	
-	override open func height(constrainedTo size: CGSize, in tableView: UITableView) -> CGFloat? {
-		return estimatedHeight
-	}
-	
-	override open var estimatedHeight: CGFloat? {
-		switch type {
-		case .quiz:
-			return 180
-		case .app:
-			return 130
-		case .link:
-			return 120
-		case .badge:
-			return 192
-		default:
-			return 160
-		}
-	}
-	
-	override open var cellClass: UITableViewCell.Type? {
-		
-		switch type {
-		case .quiz:
-			return StormObjectFactory.shared.class(for: NSStringFromClass(QuizBadgeScrollerViewCell.self)) as? UITableViewCell.Type ?? QuizBadgeScrollerViewCell.self
-		case .app:
-			return StormObjectFactory.shared.class(for: NSStringFromClass(AppCollectionCell.self)) as? UITableViewCell.Type ?? AppCollectionCell.self
-		case .link:
-			return StormObjectFactory.shared.class(for: NSStringFromClass(LinkCollectionCell.self)) as? UITableViewCell.Type ?? LinkCollectionCell.self
-		case .badge:
-			return StormObjectFactory.shared.class(for: NSStringFromClass(BadgeScrollerViewCell.self)) as? UITableViewCell.Type ?? BadgeScrollerViewCell.self
-		default:
-			return nil
-		}
-	}
-	
-	override open var selectionStyle: UITableViewCell.SelectionStyle? {
-		return UITableViewCell.SelectionStyle.none
-	}
-	
-	override open var accessoryType: UITableViewCell.AccessoryType? {
-		get {
-			return UITableViewCell.AccessoryType.none
-		}
-		set {}
-	}
-	
-	override open var useNibSuperclass: Bool {
-		return false
-	}
+    }
+    
+    //MARK: -
+    //MARK: Row protocol
+    //MARK: -
+    
+    override open func configure(cell: UITableViewCell, at indexPath: IndexPath, in tableViewController: TableViewController) {
+        
+        super.configure(cell: cell, at: indexPath, in: tableViewController)
+        
+        switch type {
+        case .quiz:
+            guard let quizBadgeScrollerCell = cell as? QuizBadgeScrollerViewCell else { return }
+            quizBadgeScrollerCell.quizzes = quizzes
+            quizBadgeScrollerCell.badges = badges
+            break
+        case .app:
+            guard let appCollectionCell = cell as? AppCollectionCell else { return }
+            appCollectionCell.apps = apps
+            break
+        case .link:
+            guard let linkCollectionCell = cell as? LinkCollectionCell else { return }
+            linkCollectionCell.links = links
+            break
+        case .badge:
+            guard let badgeScrollerCell = cell as? BadgeScrollerViewCell else { return }
+            badgeScrollerCell.badges = badges
+        default: break
+        }
+    }
+    
+    override open func height(constrainedTo size: CGSize, in tableView: UITableView) -> CGFloat? {
+        return estimatedHeight
+    }
+    
+    override open var estimatedHeight: CGFloat? {
+        switch type {
+        case .quiz:
+            return 180
+        case .app:
+            return 130
+        case .link:
+            return 120
+        case .badge:
+            return 192
+        default:
+            return 160
+        }
+    }
+    
+    override open var cellClass: UITableViewCell.Type? {
+        
+        switch type {
+        case .quiz:
+            return StormObjectFactory.shared.class(for: NSStringFromClass(QuizBadgeScrollerViewCell.self)) as? UITableViewCell.Type ?? QuizBadgeScrollerViewCell.self
+        case .app:
+            return StormObjectFactory.shared.class(for: NSStringFromClass(AppCollectionCell.self)) as? UITableViewCell.Type ?? AppCollectionCell.self
+        case .link:
+            return StormObjectFactory.shared.class(for: NSStringFromClass(LinkCollectionCell.self)) as? UITableViewCell.Type ?? LinkCollectionCell.self
+        case .badge:
+            return StormObjectFactory.shared.class(for: NSStringFromClass(BadgeScrollerViewCell.self)) as? UITableViewCell.Type ?? BadgeScrollerViewCell.self
+        default:
+            return nil
+        }
+    }
+    
+    override open var selectionStyle: UITableViewCell.SelectionStyle? {
+        return UITableViewCell.SelectionStyle.none
+    }
+    
+    override open var accessoryType: UITableViewCell.AccessoryType? {
+        get {
+            return UITableViewCell.AccessoryType.none
+        }
+        set {}
+    }
+    
+    override open var useNibSuperclass: Bool {
+        return false
+    }
 }
--- ThunderCloud/ListItem.swift
+++ ThunderCloud/ListItem.swift
@@ -24,115 +24,115 @@ public protocol RowSelectable {
 /// ListItem is the base object for displaying table rows in storm.
 /// It complies to the `Row` protocol
 open class ListItem: StormObject, Row {
-	
-	open var accessoryType: UITableViewCell.AccessoryType?
-	
-	/// Whether the row should display separators when rendered in the UITableView
-	open var displaySeparators: Bool = true
-	
-	/// The title of the row
-	open var title: String?
-	
-	/// The subtitle of the row
-	/// The subtitle gets displayed under the title
-	open var subtitle: String?
-	
-	/// A `StormLink` which determines what the row does when it is selected
-	open var link: StormLink?
-	
-	/// The image for the row
-	/// This is placed on the left hand side of the cell
-	open var image: UIImage?
-	
-	/// The row's title text color
-	open var titleTextColor: UIColor?
-	
-	/// The row's detail text color
-	open var detailTextColor: UIColor?
-	
-	/// The `UINavigationController` of the view controller the row is displayed in
-	public weak var parentNavigationController: UINavigationController?
+    
+    open var accessoryType: UITableViewCell.AccessoryType?
+    
+    /// Whether the row should display separators when rendered in the UITableView
+    open var displaySeparators: Bool = true
+    
+    /// The title of the row
+    open var title: String?
+    
+    /// The subtitle of the row
+    /// The subtitle gets displayed under the title
+    open var subtitle: String?
+    
+    /// A `StormLink` which determines what the row does when it is selected
+    open var link: StormLink?
+    
+    /// The image for the row
+    /// This is placed on the left hand side of the cell
+    open var image: UIImage?
+    
+    /// The row's title text color
+    open var titleTextColor: UIColor?
+    
+    /// The row's detail text color
+    open var detailTextColor: UIColor?
+    
+    /// The `UINavigationController` of the view controller the row is displayed in
+    public weak var parentNavigationController: UINavigationController?
     
     /// The `UITableViewController` that the row is displayed in
     public weak var parentViewController: TableViewController?
-	
-	required public init(dictionary: [AnyHashable : Any]) {
-		
-		super.init(dictionary: dictionary)
-		
-		if let titleDict = dictionary["title"] as? [AnyHashable : Any] {
-			title = StormLanguageController.shared.string(for: titleDict)
-		}
-		
-		if let subtitleDict = dictionary["description"] as? [AnyHashable : Any] {
-			subtitle = StormLanguageController.shared.string(for: subtitleDict)
-		}
-		
-		image = StormGenerator.image(fromJSON: dictionary["image"])
-		
-		if let linkDicationary = dictionary["link"] as? [AnyHashable : Any] {
-			link = StormLink(dictionary: linkDicationary)
-		}
-	}
-	
-	open func configure(cell: UITableViewCell, at indexPath: IndexPath, in tableViewController: TableViewController) {
-		
-		parentNavigationController = tableViewController.navigationController
+    
+    required public init(dictionary: [AnyHashable : Any]) {
+        
+        super.init(dictionary: dictionary)
+        
+        if let titleDict = dictionary["title"] as? [AnyHashable : Any] {
+            title = StormLanguageController.shared.string(for: titleDict)
+        }
+        
+        if let subtitleDict = dictionary["description"] as? [AnyHashable : Any] {
+            subtitle = StormLanguageController.shared.string(for: subtitleDict)
+        }
+        
+        image = StormGenerator.image(fromJSON: dictionary["image"])
+        
+        if let linkDicationary = dictionary["link"] as? [AnyHashable : Any] {
+            link = StormLink(dictionary: linkDicationary)
+        }
+    }
+    
+    open func configure(cell: UITableViewCell, at indexPath: IndexPath, in tableViewController: TableViewController) {
+        
+        parentNavigationController = tableViewController.navigationController
         parentViewController = tableViewController
         
-		if link == nil {
-			cell.accessoryType = .none
-		}
-		
-		if let stormCell = cell as? StormTableViewCell {
-			stormCell.parentViewController = tableViewController
-		}
-		
-		if let titleTextColor = titleTextColor {
-			cell.textLabel?.textColor = titleTextColor
-			if let tCell = cell as? TableViewCell {
-				tCell.cellTextLabel?.textColor = titleTextColor
-			}
-		}
-		
-		if let detailTextColor = detailTextColor {
-			cell.detailTextLabel?.textColor = detailTextColor
-			if let tCell = cell as? TableViewCell {
-				tCell.cellDetailLabel?.textColor = detailTextColor
-			}
-		}
-		
-		
-		if let tableCell = cell as? TableViewCell {
+        if link == nil {
+            cell.accessoryType = .none
+        }
+        
+        if let stormCell = cell as? StormTableViewCell {
+            stormCell.parentViewController = tableViewController
+        }
+        
+        if let titleTextColor = titleTextColor {
+            cell.textLabel?.textColor = titleTextColor
+            if let tCell = cell as? TableViewCell {
+                tCell.cellTextLabel?.textColor = titleTextColor
+            }
+        }
+        
+        if let detailTextColor = detailTextColor {
+            cell.detailTextLabel?.textColor = detailTextColor
+            if let tCell = cell as? TableViewCell {
+                tCell.cellDetailLabel?.textColor = detailTextColor
+            }
+        }
+        
+        
+        if let tableCell = cell as? TableViewCell {
             
             tableCell.cellTextLabel?.isHidden = title == nil || title!.isEmpty
             tableCell.cellDetailLabel?.isHidden = subtitle == nil || subtitle!.isEmpty
             
-			tableCell.cellImageView?.isHidden = image == nil && imageURL == nil
-			tableCell.cellTextLabel?.font = ThemeManager.shared.theme.cellTitleFont
-			tableCell.cellDetailLabel?.font = ThemeManager.shared.theme.cellDetailFont
-			tableCell.textLabel?.font = ThemeManager.shared.theme.cellTitleFont
-			tableCell.detailTextLabel?.font = ThemeManager.shared.theme.cellDetailFont
-		}
-	}
-	
-	open var cellClass: UITableViewCell.Type? {
-		return StormTableViewCell.self
-	}
-	
-	open var padding: CGFloat? {
-		return 12.0
-	}
-	
-	open var useNibSuperclass: Bool {
-		return true
-	}
-	
-	open var estimatedHeight: CGFloat? {
-		return nil
-	}
-	
-	open func handleSelection(of row: Row, at indexPath: IndexPath, in tableView: UITableView) {
+            tableCell.cellImageView?.isHidden = image == nil && imageURL == nil
+            tableCell.cellTextLabel?.font = ThemeManager.shared.theme.cellTitleFont
+            tableCell.cellDetailLabel?.font = ThemeManager.shared.theme.cellDetailFont
+            tableCell.textLabel?.font = ThemeManager.shared.theme.cellTitleFont
+            tableCell.detailTextLabel?.font = ThemeManager.shared.theme.cellDetailFont
+        }
+    }
+    
+    open var cellClass: UITableViewCell.Type? {
+        return StormTableViewCell.self
+    }
+    
+    open var padding: CGFloat? {
+        return 12.0
+    }
+    
+    open var useNibSuperclass: Bool {
+        return true
+    }
+    
+    open var estimatedHeight: CGFloat? {
+        return nil
+    }
+    
+    open func handleSelection(of row: Row, at indexPath: IndexPath, in tableView: UITableView) {
         
         guard let parentNavigationController = parentNavigationController else { return }
         
@@ -152,22 +152,22 @@ open class ListItem: StormObject, Row {
         default:
             return
         }
-		
+        
         rowSelectable?.handleSelection(of: row, at: indexPath, in: tableView)
-	}
-	
-	open func height(constrainedTo size: CGSize, in tableView: UITableView) -> CGFloat? {
-		return nil
-	}
-	
-	open var selectionHandler: SelectionHandler? = { (row, wasSelection, indexPath, tableView) -> Void in
-		
-		guard let listItem = row as? ListItem, wasSelection else { return }
-		
-		listItem.handleSelection(of: row, at: indexPath, in: tableView)
-	}
-	
-	open var selectionStyle: UITableViewCell.SelectionStyle? {
+    }
+    
+    open func height(constrainedTo size: CGSize, in tableView: UITableView) -> CGFloat? {
+        return nil
+    }
+    
+    open var selectionHandler: SelectionHandler? = { (row, wasSelection, indexPath, tableView) -> Void in
+        
+        guard let listItem = row as? ListItem, wasSelection else { return }
+        
+        listItem.handleSelection(of: row, at: indexPath, in: tableView)
+    }
+    
+    open var selectionStyle: UITableViewCell.SelectionStyle? {
         return link != nil ? UITableViewCell.SelectionStyle.default : UITableViewCell.SelectionStyle.none
-	}
+    }
 }
