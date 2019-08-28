46800996f989431f570f72f60145d6f028bbba32
Ryan Bourne <ryan.bourne@3sidedcube.com>
2019-08-28 11:37:53 +0100

    #203 - changed AppCollectionItem to use overlay for the app name, instead of the non-existent name

--- ThunderCloud/AppCollectionItem.swift
+++ ThunderCloud/AppCollectionItem.swift
@@ -11,16 +11,20 @@ import Foundation
 /// A model representation of an app to be shown in a `TSCAppScrollerItemViewCell`
 open class AppCollectionItem: StormObjectProtocol {
     
-    /// The app's icon
+    /// Defines the keys used to decode this data object.
+    enum DecoderKeys: String {
+        case icon
+        case identifier
+        case overlay
+    }
+    
+    /// The app's icon - derived from "icon"
     public let appIcon: StormImage?
     
-    /// The app's name
+    /// The app's name - derived from "overlay"
     public let appName: String?
     
-    /// The app's price
-    public let appPrice: String?
-    
-    /// The app identity for the app, contains information on the URL schemes, app name, iTunes id e.t.c.
+    /// The app identity for the app, contains information on the URL schemes, app name, iTunes id e.t.c. - derived from "identifier"
     public let app: AppIdentity?
     
     /// Initialises a new instance from a CMS representation of an app
@@ -28,28 +32,22 @@ open class AppCollectionItem: StormObjectProtocol {
     /// - Parameter dictionary: Dictionary to use to initialise and populate the app
     public required init(dictionary: [AnyHashable : Any]) {
         
-        let icon = StormGenerator.image(fromJSON: dictionary["icon"])
+        let icon = StormGenerator.image(fromJSON: dictionary[DecoderKeys.icon])
         appIcon = icon
         
         var appIdentifier: AppIdentity?
         
-        if let identifier = dictionary["identifier"] as? String {
+        if let identifier = dictionary[DecoderKeys.identifier] as? String {
             appIdentifier = AppLinkController().apps.first(where: {$0.identifier == identifier})
             app = appIdentifier
         } else {
             app = nil
         }
         
-        if let nameKey = dictionary["name"] as? String {
-            appName = StormLanguageController.shared.string(forKey: nameKey) ?? appIdentifier?.name
+        if let overlay = dictionary[DecoderKeys.overlay] as? [AnyHashable: Any] {
+            appName = StormLanguageController.shared.string(for: overlay) ?? appIdentifier?.name
         } else {
             appName = app?.name
-        }
-        
-        if let priceDictionary = dictionary["overlay"] as? [AnyHashable : Any] {
-            appPrice = StormLanguageController.shared.string(for: priceDictionary)
-        } else {
-            appPrice = nil
         }
     }
 }
