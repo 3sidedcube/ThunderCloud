464d7696f15392b772b46df1b7575f77bc6ff4ca
Simon Mitchell <simon@3sidedcube.com>
2020-04-08 16:18:14 +0100

    Fixes styling of switch rows slightly

--- ThunderCloud/BundleDiagnosticsTableViewController.swift
+++ ThunderCloud/BundleDiagnosticsTableViewController.swift
@@ -88,14 +88,14 @@ class BundleDiagnosticTableViewController: TableViewController {
             }
         }
         
-        let notifyOfDownloadRow = InputSwitchRow(title: "Notify of Download Feedback", subtitle: "The app will show toast notifications with information on delta download progress", id: "feedback")
+        let notifyOfDownloadRow = StormDiagnosticsSwitchRow(title: "Notify of Download Feedback", subtitle: "The app will show toast notifications with information on delta download progress", id: "feedback")
         notifyOfDownloadRow.value = ContentController.showFeedback
         notifyOfDownloadRow.valueChangeHandler = { (value, _) in
             guard let showFeedback = value as? Bool else { return }
             ContentController.showFeedback = showFeedback
         }
         
-        let downloadOnWifiRow = InputSwitchRow(title: "Only Download on WiFi", subtitle: "The app will only download delta updates if your phone is connected to a WiFi network", id: "wifi")
+        let downloadOnWifiRow = StormDiagnosticsSwitchRow(title: "Only Download on WiFi", subtitle: "The app will only download delta updates if your phone is connected to a WiFi network", id: "wifi")
         downloadOnWifiRow.value = ContentController.onlyDownloadOverWifi
         downloadOnWifiRow.valueChangeHandler = { (value, _) in
             guard let onlyDownloadOverWifi = value as? Bool else { return }
---
+++ ThunderCloud/StormDiagnosticsSwitchRow.swift
@@ -0,0 +1,21 @@
+//
+//  StormDiagnosticsSwitchRow.swift
+//  ThunderCloud
+//
+//  Created by Simon Mitchell on 08/04/2020.
+//  Copyright © 2020 threesidedcube. All rights reserved.
+//
+
+import UIKit
+import ThunderTable
+
+class StormDiagnosticsSwitchRow: InputSwitchRow {
+        
+    override func configure(cell: UITableViewCell, at indexPath: IndexPath, in tableViewController: TableViewController) {
+                
+        guard let tableCell = cell as? InputSwitchViewCell else { return }
+        
+        tableCell.cellTextLabel?.font = ThemeManager.shared.theme.dynamicFont(ofSize: 17, textStyle: .body, weight: .bold)
+        tableCell.cellDetailLabel?.font = ThemeManager.shared.theme.dynamicFont(ofSize: 15, textStyle: .body)
+    }
+}
