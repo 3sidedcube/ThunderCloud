bbab0c2bb0db1fb36b53b4dad8a0353f45ab4d0d
Simon Mitchell <simon@3sidedcube.com>
2020-09-08 16:19:04 +0100

    Adds in-memory cache for show debug localisations bool

--- ThunderCloud/LocalisationsTool.swift
+++ ThunderCloud/LocalisationsTool.swift
@@ -21,12 +21,20 @@ class LocalisationsTool: DiagnosticTool {
         navigationController.show(tableViewController, sender: nil)
     }
     
+    private static var _showDebugLocalisations: Bool?
+    
     /// Whether to render localisation key along with the value for debugging purposes
     static internal var showDebugLocalisations: Bool {
         get {
-            return UserDefaults.standard.bool(forKey: "storm_debug_localisations")
+            if let _showDebugLocalisations = _showDebugLocalisations {
+                return _showDebugLocalisations
+            }
+            let defaultsShowDebugLocalisations = UserDefaults.standard.bool(forKey: "storm_debug_localisations")
+            _showDebugLocalisations = defaultsShowDebugLocalisations
+            return defaultsShowDebugLocalisations
         }
         set {
+            _showDebugLocalisations = newValue
             UserDefaults.standard.set(newValue, forKey: "storm_debug_localisations")
         }
     }
