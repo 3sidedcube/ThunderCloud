908290d5b6c58ed05954dffe392b3910392e21bc
Ben Shutt <ben@3sidedcube.com>
2020-09-21 15:34:09 +0100

    Reident `EditLocalisationTableViewCell.swift`

--- ThunderCloud/EditLocalisationTableViewCell.swift
+++ ThunderCloud/EditLocalisationTableViewCell.swift
@@ -12,50 +12,50 @@ import ThunderTable
 
 /// A cell for allowing the user to edit a localisation for a certain language
 class EditLocalisationTableViewCell: InputTextViewCell {
-
-	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-		super.init(style: style, reuseIdentifier: reuseIdentifier)
-		setup()
-	}
-	
-	required init?(coder aDecoder: NSCoder) {
-		super.init(coder: aDecoder)
-	}
-	
-	override func awakeFromNib() {
-		super.awakeFromNib()
-		setup()
-	}
-	
-	private var separatorView: UIView?
-	
-	private func setup() {
-		
-		contentView.backgroundColor = .clear
-		backgroundColor = .clear
-		
-		backgroundView = UIView()
-		backgroundView?.backgroundColor = .white
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    private var separatorView: UIView?
+    
+    private func setup() {
+        
+        contentView.backgroundColor = .clear
+        backgroundColor = .clear
+        
+        backgroundView = UIView()
+        backgroundView?.backgroundColor = .white
         backgroundView?.layer.borderWidth = 1/UIScreen.main.scale
         backgroundView?.layer.borderColor = UIColor(hexString: "9B9B9B")?.cgColor
-		backgroundView?.layer.cornerRadius = 2.0
-		
-		contentView.addSubview(backgroundView!)
-		contentView.sendSubviewToBack(backgroundView!)
-		
-		separatorView = UIView()
-		separatorView?.backgroundColor = UIColor(hexString: "9B9B9B")
-		backgroundView!.addSubview(separatorView!)
-	}
-	
-	override func layoutSubviews() {
-		super.layoutSubviews()
-		
-		backgroundView?.frame = CGRect(x: 8, y: 8, width: contentView.bounds.width-16, height: contentView.bounds.height - 8)
-		guard let cellTextLabelContainer = cellTextLabel?.superview else {
-			separatorView?.frame = .zero
-			return
-		}
-		separatorView?.frame = CGRect(x: cellTextLabelContainer.frame.maxX, y: 0, width: 1/UIScreen.main.scale, height: backgroundView?.frame.height ?? 0)
-	}
+        backgroundView?.layer.cornerRadius = 2.0
+        
+        contentView.addSubview(backgroundView!)
+        contentView.sendSubviewToBack(backgroundView!)
+        
+        separatorView = UIView()
+        separatorView?.backgroundColor = UIColor(hexString: "9B9B9B")
+        backgroundView!.addSubview(separatorView!)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        backgroundView?.frame = CGRect(x: 8, y: 8, width: contentView.bounds.width-16, height: contentView.bounds.height - 8)
+        guard let cellTextLabelContainer = cellTextLabel?.superview else {
+            separatorView?.frame = .zero
+            return
+        }
+        separatorView?.frame = CGRect(x: cellTextLabelContainer.frame.maxX, y: 0, width: 1/UIScreen.main.scale, height: backgroundView?.frame.height ?? 0)
+    }
 }
